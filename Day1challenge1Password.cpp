8aa29f44d4e9e0b76265b84b4d44cc1169c9592b
Alexander Peterson <abp6609@gmail.com>
2026-07-25 15:44:51 -0400

    trying to solve another challenge

--- Day1challenge1Password.cpp
+++ Day1challenge1Password.cpp
@@ -3,17 +3,16 @@
 #include <fstream>
 #include <vector>
 #include <list>
-
 using namespace std;
 
 int main()
 {
 
-    ifstream inputFile("passwords.txt");
+    ifstream inputFile("exampleTest.txt");
     string line;
-    int startCode = 50;
     int password = 0;
-    int previousCode = 0;
+    int previousCode = 50;
+    int result = 0;
     while(getline(inputFile, line))
     {
         string num = line.substr(1, line.size() - 1);
@@ -27,43 +26,36 @@ int main()
          
         //cout << "Start Code: " << startCode << endl;
         cout << "Value: " << value << endl;
-        previousCode = startCode;
-        startCode += value;    
+        result = previousCode + value;    
         cout << "Previous Code: " << previousCode << endl;
         
         //Wrap around logic
-        startCode = (startCode % 100);
-        
-        if(startCode > 99){
-            //password += 1;
-            if(previousCode + value > 99){
-                password += 1*((previousCode + value) / 100);
-            }
-            else{
-                password += 1;
-            }
+        // startCode = (startCode % 100);
+        if((result == 0) || (result == 100))
+        {
+            password += 1;
         }
-        if(startCode < 0 && previousCode != 0){
-           if(previousCode + value < 0){
-                password += 1 * (-(previousCode + value) / 100 + 1);
+        else if(result > 99){
+            password += 1 * result/100;
+            result %= 100;
+        }
+        if (result < 0){
+            
+            if (previousCode > 0)
+            {
+                password += 1 * abs(result)/100;
             }
-            else{
-                password += 1;
-            }
+            result = 100 + result;
+        }
+        cout << "result:" << result << endl;
+        if((previousCode < 0 && result > 0 )){
+           password += 1;
         }
         //Check to see if the code is zero
-        if(startCode == 0)
-        {
-
-            password += 1;
-
-  
-        }
-
-
+ 
         //cout << "Start Code: " << startCode << endl;
         cout << "Passwords Found: " << password << endl;
-        
+        previousCode = result;
     }
     inputFile.close();
 
